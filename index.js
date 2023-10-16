c34c7eac5353f780ed57ceccec77f55a46dee0ea
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-10-16 15:26:27 +0500

    Again activated bulb using another logic

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-// 1.
+// 1.Bulb on/off using Dom Manipulation
+// let bulb= document.querySelector('#bulb');
+// let btn= document.querySelector('#bb');
+
+// btn.addEventListener('click',()=>{
+//     bulb.classList.toggle('bb-bg');
+// })
+
+// 1.Bulb on/off using Dom Manipulation using if else (way#2)
+
 let bulb= document.querySelector('#bulb');
 let btn= document.querySelector('#bb');
 
-btn.addEventListener('click',()=>{
-    bulb.classList.toggle('bb-bg');
-})
+let value = 0;
 
+document.querySelector('#bb').addEventListener('click',()=>{
+
+    if (value==0) 
+    {
+        bulb.style.backgroundColor = "yellow";
+        value=1;
+    } 
+    else 
+    {
+        bulb.style.backgroundColor = null;
+        value=0;    
+    }
+    // Ya aik conditional statement hai is mai ya tooh sirf "if" chalay ga ya tooh "else" chalaay ga
+})
 
