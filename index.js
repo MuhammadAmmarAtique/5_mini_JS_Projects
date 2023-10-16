17d9489b85acf91919b440f105555efaf64a1390
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-10-16 18:34:25 +0500

    Learned difference b/w .innerHtml vs .TextContent

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// 1.Bulb on/off using Dom Manipulation
+// 1.(a) Bulb on/off using Dom Manipulation
+
 // let bulb= document.querySelector('#bulb');
 // let btn= document.querySelector('#bb');
 
@@ -6,25 +7,31 @@
 //     bulb.classList.toggle('bb-bg');
 // })
 
-// 1.Bulb on/off using Dom Manipulation using if else (way#2)
+// 1. (b) Bulb on/off using Dom Manipulation using if else (way#2)
 
-let bulb= document.querySelector('#bulb');
-let btn= document.querySelector('#bb');
+// let bulb= document.querySelector('#bulb');
+// let btn= document.querySelector('#bb');
 
-let value = 0;
+// let value = 0;
 
-document.querySelector('#bb').addEventListener('click',()=>{
+// document.querySelector('#bb').addEventListener('click',()=>{
 
-    if (value==0) 
-    {
-        bulb.style.backgroundColor = "yellow";
-        value=1;
-    } 
-    else 
-    {
-        bulb.style.backgroundColor = null;
-        value=0;    
-    }
-    // Ya aik conditional statement hai is mai ya tooh sirf "if" chalay ga ya tooh "else" chalaay ga
-})
+//     if (value==0) 
+//     {
+//         bulb.style.backgroundColor = "yellow";
+//         value=1;
+//     } 
+//     else 
+//     {
+//         bulb.style.backgroundColor = null;
+//         value=0;    
+//     }
+//     // Ya aik conditional statement hai is mai ya tooh sirf "if" chalay ga ya tooh "else" chalaay ga
+// })
+
+// 2. .innerHTML vs .textContent
+
+document.querySelector('#box').innerHTML="<h1>hello</h1>";
+
+document.querySelector('#box2').textContent="<h1>hello</h1>";
 
