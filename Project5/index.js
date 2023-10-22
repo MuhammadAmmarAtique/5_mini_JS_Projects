b25bd0287d15125f4637084d7e14b4cd8a787e08
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-10-22 21:07:23 +0500

    added comments

--- Project5/index.js
+++ Project5/index.js
@@ -1,4 +1,4 @@
-// Storing dp and stories in a array to access them later
+// Storing dp and stories in a array of objects to access them later
 let arr = [
     { dp: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWFsZSUyMG1vZGVsc3xlbnwwfDF8MHx8fDI%3D", story: "https://images.unsplash.com/photo-1433086966358-54859d0ed716?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8MXwwfHx8Mg%3D%3D" },
 
@@ -22,18 +22,22 @@ document.querySelector("#stories").innerHTML +=
 
 });
 
-// 
+// Applying bubble event
 
 document.querySelector('#stories').addEventListener('click',(element)=>{
     // console.log(element.target); (it is the element we are clicking on)
     // console.log(element.target.id); (it will get the element's id like 0,1,2 )
 
-//    console.log(arr[element.target.id]); //put that numbering inside array as a index to access array elements
-//    console.log(arr[element.target.id].story); //to get story inside array
+   //console.log(arr[element.target.id]); //put that numbering inside array as a index to access array elements
+  //console.log(arr[element.target.id].story); //to get story inside array
 
 document.querySelector('#show_stories').style.display ='block';
 document.querySelector('#show_stories').style.backgroundImage = 'url(' + arr[element.target.id].story + ')';
 
+// After some time story will disappear again
+setTimeout(() => {
+    document.querySelector('#show_stories').style.display ='none';
+}, 3000);
 
 })
 
